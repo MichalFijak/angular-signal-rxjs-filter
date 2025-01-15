bb57c6536cb271c3068c3688f910cba9db552cec
MichalFijak <michalfijak1@hotmail.com>
2025-01-15 19:17:22 +0100

    first letter is uppercased

--- src/pipe/country-formatter.pipe.ts
+++ src/pipe/country-formatter.pipe.ts
@@ -9,14 +9,30 @@ export class CountryFormatterPipe implements PipeTransform {
   constructor(private sanitizer:DomSanitizer){}
   transform(value: string, filter:string): SafeHtml {
     let lowerCasedValue = value.toLowerCase();
+    let lowerCasedFilter=filter.toLowerCase();
     if(filter.length>0)
     {
+
     let formattedValue='';
-    let index  = lowerCasedValue.indexOf(filter);
-    let x = lowerCasedValue.substring(filter.length);
-    console.log(index);
-    formattedValue =formattedValue+'<strong>'+filter+'</strong>';
-    formattedValue= formattedValue + x;
+    let index  = lowerCasedValue.indexOf(lowerCasedFilter);
+    let beforeMatch = lowerCasedValue.substring(0,index);
+    let afterMatch = lowerCasedValue.substring(lowerCasedFilter.length+index);
+
+    formattedValue += beforeMatch[0].toUpperCase() + beforeMatch.substring(1);
+    let splittedWords=lowerCasedValue.split('');
+
+    if(index===0 || index=== splittedWords.indexOf(' ')+1)
+    {
+      formattedValue +='<strong>'+lowerCasedFilter.toLocaleUpperCase()+'</strong>';
+
+    }
+    else
+    {
+      formattedValue +='<strong>'+lowerCasedFilter+'</strong>';
+    }
+
+    formattedValue += afterMatch;
+
     return this.sanitizer.bypassSecurityTrustHtml(formattedValue);
     }
     else
